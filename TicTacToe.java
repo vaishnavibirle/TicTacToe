3b25b9edffba050afaa1f39abecde8704a994006
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-31 08:45:07 +0530

    adding UC1

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,28 @@
 package com.company;
-
+import java.util.Scanner;
 public class TicTacToe {
-
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("WELCOME TO TIC TAC TOE PROGRAM");
+        //TicTacToe Game board 3*3
+        char[][] board = new char[3][3];
+        //design board with dashes
+        for(int i = 0 ;i < 3 ; i++)  {
+            for(int j=0;j<3;j++){
+                board[i][j] = '-';
+            }
+        }
+        //Board
+        printBoard(board);
     }
+    //for printing
+    public static void printBoard(char[][] board) {
+        //i represents the row
+        for (int i=0;i<3;i++) {
+            // j represents the column
+            for (int j=0;j<3;j++) {
+                System.out.print(board[i][j]); //for not printing to in oneline
+            }
+            System.out.println();
+        }
+    }
+
 }
