63486e2f5adb2cf4a7964cce0d326719832c737d
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-31 14:36:47 +0530

    Choosing X or O

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,28 +1,53 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
+        //welcome message
+        System.out.println("Welcome to Tic Tac Toe Game");
         //TicTacToe Game board 3*3
         char[][] board = new char[3][3];
-        //design board with dashes
-        for(int i = 0 ;i < 3 ; i++)  {
-            for(int j=0;j<3;j++){
+        //create board with dashes
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 board[i][j] = '-';
             }
         }
         //Board
         printBoard(board);
+        choose();
+
     }
+
     //for printing
     public static void printBoard(char[][] board) {
         //i represents the row
-        for (int i=0;i<3;i++) {
+        for (int i = 0; i < 3; i++) {
             // j represents the column
-            for (int j=0;j<3;j++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j]); //for not printing to in oneline
             }
             System.out.println();
         }
     }
+    //creating a method to choose between X or O
+    public static void choose() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Choose from X or O");
+        String game = s.next();
+        char player = 0;
+        char computer;
+        if (player == 'X') {
+            computer = 'O';
+        } else {
+            computer = 'X';
+        }
+    }
+
+    public static void playGame() {
+
+    }
+
 
 }
